bb708cb919d9716662096938286adbce1a36dacc
nabeelz <nabeelz@icenginc.com>
2019-03-04 16:36:54 -0800

    Initial commit. Can detect devices

--- I2C Monitor Module/Form1.cs
+++ I2C Monitor Module/Form1.cs
@@ -19,6 +19,8 @@ namespace I2C_Monitor_Module
 			TotalPhase iface = new TotalPhase(); //each totalphase is a new pair, create more if more devices
 			iface.find_device(); //button to do this, then display devices
 
+			
+
 			//select devices to use
 			//maybe some config file to define the devices? so we can make sure it is the right pair
 
