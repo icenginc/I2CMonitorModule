bacfee3235bfa6205cdd8a4fb4541fd52edb68a9
nabeelz <nabeelz@icenginc.com>
2019-03-05 16:35:30 -0800

    Can scan and select devices

--- I2C Monitor Module/Form1.Designer.cs
+++ I2C Monitor Module/Form1.Designer.cs
@@ -28,20 +28,117 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button_scan = new System.Windows.Forms.Button();
+			this.button_select = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox_aardvark = new System.Windows.Forms.ComboBox();
+			this.comboBox_beagle = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox_active = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
+			// 
+			// button_scan
+			// 
+			this.button_scan.Location = new System.Drawing.Point(269, 158);
+			this.button_scan.Name = "button_scan";
+			this.button_scan.Size = new System.Drawing.Size(75, 23);
+			this.button_scan.TabIndex = 0;
+			this.button_scan.Text = "Scan";
+			this.button_scan.UseVisualStyleBackColor = true;
+			this.button_scan.Click += new System.EventHandler(this.button_scan_Click);
+			// 
+			// button_select
+			// 
+			this.button_select.Location = new System.Drawing.Point(350, 158);
+			this.button_select.Name = "button_select";
+			this.button_select.Size = new System.Drawing.Size(75, 23);
+			this.button_select.TabIndex = 1;
+			this.button_select.Text = "Select";
+			this.button_select.UseVisualStyleBackColor = true;
+			this.button_select.Click += new System.EventHandler(this.button_select_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(283, 50);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(101, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Available Aardvarks";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(286, 96);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Available Beagles";
+			// 
+			// comboBox_aardvark
+			// 
+			this.comboBox_aardvark.FormattingEnabled = true;
+			this.comboBox_aardvark.Location = new System.Drawing.Point(286, 67);
+			this.comboBox_aardvark.Name = "comboBox_aardvark";
+			this.comboBox_aardvark.Size = new System.Drawing.Size(121, 21);
+			this.comboBox_aardvark.TabIndex = 4;
+			// 
+			// comboBox_beagle
+			// 
+			this.comboBox_beagle.FormattingEnabled = true;
+			this.comboBox_beagle.Location = new System.Drawing.Point(286, 113);
+			this.comboBox_beagle.Name = "comboBox_beagle";
+			this.comboBox_beagle.Size = new System.Drawing.Size(121, 21);
+			this.comboBox_beagle.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 12);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Devices";
+			// 
+			// listBox_active
+			// 
+			this.listBox_active.FormattingEnabled = true;
+			this.listBox_active.Location = new System.Drawing.Point(12, 29);
+			this.listBox_active.Name = "listBox_active";
+			this.listBox_active.Size = new System.Drawing.Size(120, 95);
+			this.listBox_active.TabIndex = 8;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(437, 261);
+			this.Controls.Add(this.listBox_active);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox_beagle);
+			this.Controls.Add(this.comboBox_aardvark);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button_select);
+			this.Controls.Add(this.button_scan);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button_scan;
+		private System.Windows.Forms.Button button_select;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox_aardvark;
+		private System.Windows.Forms.ComboBox comboBox_beagle;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox listBox_active;
 	}
 }
 
--- I2C Monitor Module/Form1.cs
+++ I2C Monitor Module/Form1.cs
@@ -12,12 +12,14 @@ namespace I2C_Monitor_Module
 {
 	public partial class Form1 : Form
 	{
+		TotalPhase iface = new TotalPhase(); //one totalphase, redefine the current devices as the selection changes
+
 		public Form1()
 		{
 			InitializeComponent();
 
-			TotalPhase iface = new TotalPhase(); //each totalphase is a new pair, create more if more devices
-			iface.find_device(); //button to do this, then display devices
+			button_scan_Click(this, new EventArgs());
+			
 
 			
 
@@ -26,6 +28,45 @@ namespace I2C_Monitor_Module
 
 			//read test
 			
+		}
+
+		private void button_scan_Click(object sender, EventArgs e)
+		{
+			iface.find_device(); //button to do this, then display devices
+
+			var aardvark_ids = iface.aardvarks.Select(a => a.return_id().ToString()).ToArray();
+			var beagle_ids = iface.beagles.Select(a => a.return_id().ToString()).ToArray();
+
+			comboBox_aardvark.Items.AddRange(aardvark_ids);
+			comboBox_beagle.Items.AddRange(beagle_ids);
+
+			listBox_active.Items.AddRange(aardvark_ids);
+			listBox_active.Items.AddRange(beagle_ids);
+			listBox_active.SelectionMode = SelectionMode.MultiExtended;
+		}
+
+		private void button_select_Click(object sender, EventArgs e)
+		{
+			foreach (Aardvark a in iface.aardvarks)
+			{
+				if (a.return_id().ToString() == (string)comboBox_aardvark.SelectedItem)
+					iface.current_aardvark = a;
+			}
+
+			foreach (Beagle b in iface.beagles)
+			{
+				if (b.return_id().ToString() == (string)comboBox_beagle.SelectedItem)
+					iface.current_beagle = b;
+			}
+
+			for(int i = 0; i < listBox_active.Items.Count; i++)
+			{
+				string l = listBox_active.Items[i].ToString();
+				if (l == iface.current_aardvark.return_id().ToString())
+					listBox_active.SelectedItem = l;
+				if (l == iface.current_beagle.return_id().ToString())
+					listBox_active.SelectedItem = l;
+			}
 		}
 	}
 
